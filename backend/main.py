ce7cfb0d7d6beb5e1ece8033809178bcb028d134
FrozenOJuice <79174905+FrozenOJuice@users.noreply.github.com>
2025-09-26 13:39:06 -0700

    good to Docker

---
+++ backend/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from backend.data_manager import load_movies
+
+app = FastAPI()
+
+app.add_middleware(
+   CORSMiddleware,
+   allow_origins=["http://localhost:3000"],  # Next.js dev server
+   allow_credentials=True,
+   allow_methods=["*"],
+   allow_headers=["*"],
+)
+
+@app.get("/")
+async def read_root():
+   return {"message": "root route!!"}
+
+@app.get("/ping")
+async def ping():
+   return {"message": "pong"}
+
+@app.get("/movies")
+async def get_movies():
+   return load_movies()
+
+@app.get("/movies/{movie_id}")
+async def get_movie(movie_id: int):
+   movies = load_movies()
+   for movie in movies:
+       if movie["id"] == movie_id:
+           return movie
+   raise HTTPException(status_code=404, detail="Movie not found")
